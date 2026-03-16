b03b2c641e5364d5ccde35621b8bd93320d49f96
ryanzola <ryanzola@me.com>
2026-03-15 21:26:36 -0400

    Fix: change main h-full to min-h-full to allow page scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ function App() {
         <Menu {...store} />
         <Header {...store} />
         <CommandPalette />
-        <main id="main-content" className="h-full">
+        <main id="main-content" className="min-h-full">
           <AppRoutes store={store} />
         </main>
       </Router>
